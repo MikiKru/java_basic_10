fb6c645007580f5b9cc76f3fefc6ec254b042c7c
MikiKru <michal_kruczkowski@o2.pl>
2019-10-05 10:29:52 +0200

    lambda&stream v2

--- src/main/java/lambda_stream/LambdaDemo.java
+++ src/main/java/lambda_stream/LambdaDemo.java
@@ -1,6 +1,8 @@
 package lambda_stream;
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 public class LambdaDemo {
@@ -16,9 +18,35 @@ public class LambdaDemo {
         // wypisanie wszystkich elementów listy
         // lambda predicate -> CTRL + Space
         numbers.stream().forEach(integer -> System.out.print(integer + " "));
+        System.out.println();
         numbers.forEach(integer -> System.out.print(integer + " "));
+        System.out.println();
+        // znajdź wartość max liście I sposób
+        List<Integer> maxValue = numbers.stream()
+                // sortowanie wykorzystuje klasę Comparator(podstawa sortowania)
+                // reversed() - odwraca kolejonść
+                .sorted(Comparator.comparing(Integer::intValue).reversed())
+                // ograniczenie do jednej wartości
+                .limit(1)
+                // konwersja do listy
+                .collect(Collectors.toList());
+        System.out.println("maxValue = " + maxValue);
 
-        // znajdź wartość max liście
+        // znajdź min element w liście II sposób
+        Optional<Integer> minValue = numbers.stream().min(Integer::compareTo);
+        if(minValue.isPresent()){      // sprawdzenie czy wartośc min value jest notnull
+            System.out.println("minValueOptional: " + minValue);  // obiekt Optional<Integer>
+            System.out.println("minValue: " + minValue.get());  // get pobiera zawartość Optionala
+        }
+        // znajdź wartość średnią z elementów listy
+        OptionalDouble avgValue = numbers.stream().mapToDouble(Integer::doubleValue).average();
+        if(avgValue.isPresent()){
+            System.out.println("avgValue: " + avgValue.getAsDouble());
+        }
+        
+
 
     }
+    
+    
 }
